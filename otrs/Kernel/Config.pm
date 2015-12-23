bb6e055c5b022df55b27def9400deb375c26ec0e
Diego Araujo <diegoaraujo@usp.br>
2015-12-23 08:57:45 -0200

    Create Config.pm

---
+++ otrs/Kernel/Config.pm
@@ -0,0 +1,17 @@
+# Configuração, arquivo Kernel/Config.pm
+
+# Agents
+$Self->{'AuthModule'} = 'Kernel::System::Auth::HTTPBasicAuth';
+
+# Customer Auth
+$Self->{'Customer::AuthModule'} = 'Kernel::System::CustomerAuth::HTTPBasicAuthShib';
+
+# Uncomment to override the environment vars to be used
+$Self->{'Customer::AuthModule::HTTPBasicAuthShib::UsernameEnvVar'} = 'eppn';
+$Self->{'Customer::AuthModule::HTTPBasicAuthShib::MailEnvVar'} = 'mail';
+$Self->{'Customer::AuthModule::HTTPBasicAuthShib::FirstNameEnvVar'} = 'givenName';
+$Self->{'Customer::AuthModule::HTTPBasicAuthShib::LastNameEnvVar'} = 'surname';
+$Self->{'Customer::AuthModule::HTTPBasicAuthShib::CustomerIDEnvVar'} = 'uid';
+
+#Uncomment this line case you not set HTTPBasicAuthShib::MailEnvVar or may not guarantee uniqueness email
+#$Self->{CustomerUser}->{CustomerUserEmailUniqCheck} = 0;
